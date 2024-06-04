2392bb751e0c352d5266dbbbe9c13ce1458bd15f
Ilya Bekmansurov <90272601+bekmnsrw@users.noreply.github.com>
2024-06-05 00:25:11 +0300

    Android fix (#10)
    * Added showing snackbar on error
    
    * Moved strings to resources
    
    * Added button widget
    
    * Added text field widget
    
    * Fixed bug with reloading data on HomeScreen after navigation
    
    * Removed HomeScreen sticky header

--- androidApp/src/main/java/kfu/itis/freshnews/android/feature/home/HomeView.kt
+++ androidApp/src/main/java/kfu/itis/freshnews/android/feature/home/HomeView.kt
@@ -1,26 +1,22 @@
 package kfu.itis.freshnews.android.feature.home
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -32,7 +28,6 @@ import androidx.compose.ui.unit.dp
 import kfu.itis.freshnews.android.R
 import kfu.itis.freshnews.android.designsystem.theme.ThemeProvider
 import kfu.itis.freshnews.android.utils.ColumnSpacer
-import kfu.itis.freshnews.android.utils.LazyColumnSpacer
 import kfu.itis.freshnews.android.utils.toDate
 import kfu.itis.freshnews.android.widget.FreshNewsButton
 import kfu.itis.freshnews.android.widget.FreshNewsImage
@@ -61,7 +56,6 @@ fun HomeView(
 }
 
 @Composable
-@OptIn(ExperimentalFoundationApi::class)
 private fun HomeContent(
     scaffoldPadding: PaddingValues,
     state: HomeState,
@@ -70,90 +64,77 @@ private fun HomeContent(
     if (state.error is UnknownHostException) {
         NoInternetConnection { eventHandler(HomeEvent.OnOpenWiFiSettingsClick) }
     } else {
-        LazyColumn(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(scaffoldPadding),
         ) {
-            LazyColumnSpacer(36.dp)
-
-            item { LatestNewsTitle() }
-
-            item {
-                when (state.isLatestArticlesLoading) {
-                    true -> LatestNewsLoadingView()
-                    false -> LatestNews(
-                        latestNews = state.latestArticles,
-                        onArticleClick = { eventHandler(HomeEvent.OnArticleClick(it)) },
-                    )
-                }
-            }
-
-            LazyColumnSpacer(16.dp)
-
-            stickyHeader {
-                NewsCategories(
-                    selectedCategory = state.selectedArticleCategory,
-                    onCategoryClick = { eventHandler(HomeEvent.OnArticleCategoryClick(it)) },
-                )
-            }
-
-            LazyColumnSpacer(8.dp)
-
-            when (state.isArticlesOfCategoryLoading) {
-                true -> NewsOfCategoryLoadingView()
-                false -> NewsOfCategory(
-                    articlesByCategory = state.articlesOfCategory,
-                    onArticleClick = { eventHandler(HomeEvent.OnArticleClick(it)) },
-                )
-            }
-
-            LazyColumnSpacer(80.dp)
-        }
-    }
-}
-
-@Composable
-private fun LatestNewsTitle() {
-    Column {
-        Text(
-            modifier = Modifier
-                .padding(
-                    bottom = 4.dp,
-                    start = 16.dp,
-                ),
-            text = stringResource(R.string.latest_news),
-            style = ThemeProvider.typography.screenHeading,
-            color = ThemeProvider.colors.mainText,
-        )
-    }
-}
-
-@Composable
-private fun LatestNews(
-    latestNews: List<Article>,
-    onArticleClick: (Article) -> Unit,
-) {
-    LazyRow(
-        modifier = Modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(horizontal = 16.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
-        items(
-            items = latestNews,
-            key = { article -> article.title },
-        ) { article ->
-            NewsItem(
-                modifier = Modifier.width(320.dp),
-                article = article,
-                onClick = { onArticleClick(article) },
+            LatestNewsTitle()
+            LatestArticlesList(
+                isLoading = state.isLatestArticlesLoading,
+                latestArticles = state.latestArticles,
+                onArticleClick = { article -> eventHandler(HomeEvent.OnArticleClick(article)) },
+            )
+            ColumnSpacer(8.dp)
+            NewsCategoryList(
+                selectedCategory = state.selectedArticleCategory,
+                onCategoryClick = { category -> eventHandler(HomeEvent.OnArticleCategoryClick(category)) },
+            )
+            ColumnSpacer(2.dp)
+            ArticlesOfCategory(
+                isLoading = state.isArticlesOfCategoryLoading,
+                articlesOfCategory = state.articlesOfCategory,
+                onArticleClick = { article -> eventHandler(HomeEvent.OnArticleClick(article)) },
             )
         }
     }
 }
 
 @Composable
-private fun NewsCategories(
+private fun LatestNewsTitle() {
+    Text(
+        modifier = Modifier
+            .padding(
+                top = 4.dp,
+                bottom = 4.dp,
+                start = 24.dp,
+            ),
+        text = stringResource(R.string.latest_news),
+        style = ThemeProvider.typography.screenHeading,
+        color = ThemeProvider.colors.mainText,
+    )
+}
+
+@Composable
+private fun LatestArticlesList(
+    isLoading: Boolean,
+    latestArticles: List<Article>,
+    onArticleClick: (Article) -> Unit,
+) {
+    if (isLoading) {
+        LatestNewsLoadingView()
+    } else {
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(
+                items = latestArticles,
+                key = { article -> article.title },
+            ) { article ->
+                NewsItem(
+                    modifier = Modifier.width(320.dp),
+                    article = article,
+                    onClick = { onArticleClick(article) },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun NewsCategoryList(
     selectedCategory: ArticleCategory,
     onCategoryClick: (ArticleCategory) -> Unit,
 ) {
@@ -167,22 +148,51 @@ private fun NewsCategories(
         stringResource(R.string.technology) to ArticleCategory.TECHNOLOGY,
     )
 
-    Surface(color = ThemeProvider.colors.background) {
-        LazyRow(
+    LazyRow(
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .fillMaxWidth(),
+        contentPadding = PaddingValues(horizontal = 24.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(
+            items = categories,
+            key = { category -> category.second },
+        ) { category ->
+            NewsCategoryItem(
+                isSelected = selectedCategory == category.second,
+                name = category.first,
+                onClick = { onCategoryClick(category.second) },
+            )
+        }
+    }
+}
+
+@Composable
+private fun ArticlesOfCategory(
+    isLoading: Boolean,
+    articlesOfCategory: List<Article>,
+    onArticleClick: (Article) -> Unit,
+) {
+    if (isLoading) {
+        NewsOfCategoryLoadingView()
+    } else {
+        LazyColumn(
             modifier = Modifier
-                .padding(vertical = 8.dp)
-                .fillMaxWidth(),
-            contentPadding = PaddingValues(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                .fillMaxWidth()
+                .padding(bottom = 80.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
         ) {
             items(
-                items = categories,
-                key = { category -> category.second },
-            ) { category ->
-                NewsCategoryItem(
-                    isSelected = selectedCategory == category.second,
-                    name = category.first,
-                    onClick = { onCategoryClick(category.second) },
+                items = articlesOfCategory,
+                key = { article -> article.title },
+            ) { article ->
+                NewsItem(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    article = article,
+                    onClick = { onArticleClick(article) },
                 )
             }
         }
@@ -216,25 +226,6 @@ private fun NewsCategoryItem(
             color = ThemeProvider.colors.mainText,
             style = ThemeProvider.typography.category,
         )
-    }
-}
-
-private fun LazyListScope.NewsOfCategory(
-    articlesByCategory: List<Article>,
-    onArticleClick: (Article) -> Unit,
-) {
-    items(
-        items = articlesByCategory,
-        key = { article -> article.title },
-    ) { article ->
-        NewsItem(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            article = article,
-            onClick = { onArticleClick(article) },
-        )
-        Spacer(Modifier.height(24.dp))
     }
 }
 
